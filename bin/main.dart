5fb8e2b7e540b7f6ecd6c69acde01b3459fc89f7
Faysal Ahmed <19101043@uap-bd.edu>
2025-06-21 23:08:15 +0600

    Dart sytax

---
+++ bin/main.dart
@@ -0,0 +1,14 @@
+void main(){
+  print('hello world dart');
+
+  var name = 'Faysal Ahmed';
+  print (name);
+  var number = 100;
+  print(number);
+
+  int num1 = 1000;
+  print(num1);
+  String name1 = 'Shrabon Ahmed';
+  print(name1);
+
+}
