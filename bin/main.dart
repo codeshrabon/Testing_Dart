9787d0a33bb3efebe3ba08c7940c1e87de57e14a
Faysal Ahmed <19101043@uap-bd.edu>
2025-06-23 21:26:53 +0600

    Another changes

--- bin/main.dart
+++ bin/main.dart
@@ -1,17 +1,75 @@
-void main(){
-  print('hello world dart');
+// import 'dart:html_common';
 
-  var name = 'Faysal Ahmed';
-  print (name);
-  var number = 100;
-  print(number);
+void main() {
+  // print('hello world dart');
 
-  int num1 = 1000;
-  print(num1);
-  String name1 = 'Shrabon Ahmed';
-  print(name1);
+  // var name = 'Faysal Ahmed';
+  // print (name);
+  // var number = 100;
+  // print(number);
 
-  bool youreChoice = false;
+  // int num1 = 1000;
+  // print(num1);
+  // String name1 = 'Shrabon Ahmed';
+  // print(name1);
 
-  print(youreChoice);
+  // bool youreChoice = false;
+
+  // print(youreChoice);
+
+  // concat name
+
+  // var fName = 'Faysal';
+  // var lName = 'Ahmed';
+
+  // print(fName + lName);
+
+  // if we use dynamic instead var
+  // this will work as same as var
+
+  // var fName = "Shrabon";
+  // fName = "Ahmed";
+
+  // dynamic lName = "Ahmed";
+
+  // final middleName = "Russell";
+  // // using final keyword it means this data is constant
+  // //middleName = "Molla";
+
+  // print(fName);
+  // print(lName);
+  // print(fName + " " + lName);
+  // print(middleName);
+
+  // for(var i=0; i<10; i++){
+  //   var count =1+i;
+  //   // print( "$count Shrabon");
+
+  //   if (i==6)
+  //   continue;
+  //   print(count.toString() + ". Shrabon");
+  //   print("${i}. Faysal");
+  // }
+
+  var name = new List.filled(3, '', growable: false);
+  name[0] = "faysal";
+  name[1] = "ahmed";
+  print(name);
+
+  var nameList = <String>[];
+  
+  for (var i = 0; i < 4; i++) {
+    //nameList.add("Russell");
+    nameList.add("Faysal");
+    print('$i => Faysal ');
+  }
+  //var nameList = <String>[];
+
+  //List<String> nameList = [];
+  // nameList.add("Russell");
+  // nameList.add("Russell");
+  // nameList.add("Russell");
+  // nameList.add("Russell");
+
+  // print(nameList);
 }
