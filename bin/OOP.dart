0c5c963c015954bccaffaeda19ce903e59cc2d3a
Faysal Ahmed <19101043@uap-bd.edu>
2025-07-01 04:14:43 +0600

    New class, method, function breff

---
+++ bin/OOP.dart
@@ -0,0 +1,18 @@
+void main(){
+	var classObject = new MyClass(); // you can use new not 								//mendatory in dart 
+
+	var printNewClass = classObject.myMethod();
+	print(printNewClass);
+}
+
+
+class MyClass{
+
+	int myMethod(){
+
+    print("Faysal" );
+	return 40;
+  
+  
+}
+}
